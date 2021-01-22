c8319ada01beed16e78766bebca390b2a9e5da3d
greenwookez <greenwookez@gmail.com>
2021-01-22 21:21:23 +0300

    SignInScreen done

--- src/screens/SignInScreen/SignInScreen.jsx
+++ src/screens/SignInScreen/SignInScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, TextInput  } from 'react-native';
 import ScreenWrapper from '../../components/ScreenWrapper';
+import RecButton from '../../components/RecButton'
 import styles from './styles';
 
 const SignInScreen = ({
@@ -8,7 +9,32 @@ const SignInScreen = ({
 }) => {
   return (
     <ScreenWrapper>
-      <Text>SignInScreen</Text>
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Text style={styles.welcome}>Авторизация</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Nickname or email"
+            placeholderTextColor="#888"
+            returnKeyType={"nickname"}
+            autoCapitalize="none"
+            autoCompleteType="username"
+            autoCorrect="false"
+            keyboardAppearance="dark"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor="#888"
+            returnKeyType={"nickname"}
+            autoCapitalize="none"
+            autoCompleteType="password"
+            autoCorrect="false"
+            keyboardAppearance="dark"
+          />
+          <RecButton btnStyle={styles.button}>Войти в аккаунт</RecButton>
+        </View>
+      </View>
     </ScreenWrapper>
   )
 };
