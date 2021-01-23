d35b2e31c54508465c42f937c500d8a97d15f3ba
greenwookez <greenwookez@gmail.com>
2021-01-23 20:07:42 +0300

    Add SignOut btn functionality

--- src/components/CustomDrawer/components/Footer/Footer.jsx
+++ src/components/CustomDrawer/components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 import styles from './styles';
+import { SIGN_UP_SCREEN, SIGN_IN_SCREEN } from '../../../../routes';
+import store, { actionCreators } from '../../../../services/store';
 
 import RecButton from '../../../RecButton'
 import getIsAuthorized from '../../../../utils/getIsAuthorized';
@@ -16,9 +18,9 @@ const Footer = ({
       <View style={styles.aboutContainer}>
         <Text style={styles.about}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</Text>
       </View>
-      {isAuthorized && <RecButton onPress={() => navigation.navigate('SignUpScreen')}>Выйти</RecButton>}
-      {!isAuthorized && <RecButton onPress={() => navigation.navigate('SignInScreen')}>Войти</RecButton>}
-      {!isAuthorized && <RecButton onPress={() => navigation.navigate('SignUpScreen')}>Регистрация</RecButton>}
+      {isAuthorized && <RecButton onPress={() => { navigation.navigate(SIGN_UP_SCREEN); store.dispatch(actionCreators.setUserToken(null)) }}>Выйти</RecButton>}
+      {!isAuthorized && <RecButton onPress={() => navigation.navigate(SIGN_IN_SCREEN)}>Войти</RecButton>}
+      {!isAuthorized && <RecButton onPress={() => navigation.navigate(SIGN_UP_SCREEN)}>Регистрация</RecButton>}
     </View>
   )
 };
